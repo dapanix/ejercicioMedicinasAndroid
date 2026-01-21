41d01b8e0c6dd595fa5767b6454c1c9d46e112c9
dapanix <danielfernandezneira@gmail.com>
2026-01-21 12:42:24 +0100

    commit antes de parte Carlos

---
+++ app/src/main/java/com/example/fernandezdanielproyectofinal/EditarActivity.kt
@@ -0,0 +1,15 @@
+package com.example.fernandezdanielproyectofinal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fernandezdanielproyectofinal.databinding.ActivityMainBinding
+
+class EditarActivity : AppCompatActivity()  {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+    }
+
+}
